2e3f1dd17e95afd28f51f15695133c1270ed729b
nobu1575 <lightflat@icloud.com>
2022-10-25 23:40:14 +0900

    add ブログ編集投稿画面作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-    return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);//$postの中身を戻り値にする。
+    return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]);//$postの中身を戻り値にする。
     }
     
     public function show(Post $post)
@@ -28,5 +28,18 @@ class PostController extends Controller
        $input = $request['post'];
        $post->fill($input)->save();
        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        
+        return redirect('/posts/' . $post->id);
     }
 }
